a3811f2d785ab51ba8b396a53b06450001321e6e
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-21 15:28:20 +0530

    join

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const path = require("node:path");
 // console.log(path.format(path.parse(__filename)));
 // console.log(path.format(path.parse(__dirname)));
 
-console.log(path.isAbsolute(__filename));
-console.log(path.isAbsolute(__dirname));
-console.log(path.isAbsolute("./data.js"));
+// console.log(path.isAbsolute(__filename));
+// console.log(path.isAbsolute(__dirname));
+// console.log(path.isAbsolute("./data.js"));
+
+console.log(path.join("folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "folder2", "index.html"));
+console.log(path.join("/folder1", "folder2", "../index.html"));
+console.log(path.join(__dirname, "data.json"));
