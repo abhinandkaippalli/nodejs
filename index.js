34f037053b8441acc92987d63034c5e7742eeaf8
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-21 15:35:05 +0530

    resolve

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const path = require("node:path");
 // console.log(path.isAbsolute(__dirname));
 // console.log(path.isAbsolute("./data.js"));
 
-console.log(path.join("folder1", "folder2", "index.html"));
-console.log(path.join("/folder1", "folder2", "index.html"));
-console.log(path.join("/folder1", "folder2", "../index.html"));
-console.log(path.join(__dirname, "data.json"));
+// console.log(path.join("folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "folder2", "index.html"));
+// console.log(path.join("/folder1", "folder2", "../index.html"));
+// console.log(path.join(__dirname, "data.json"));
+
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "../index.html"));
+console.log(path.resolve(__dirname, "data.json"));
