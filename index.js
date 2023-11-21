0ee56f0a539bffc347e06c13ac36f5d6d958b448
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-21 14:35:30 +0530

    extename

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const path = require("node:path");
 console.log(__filename);
 console.log(__dirname);
 
-console.log(path.basename(__filename));
-console.log(path.basename(__dirname));
+// console.log(path.basename(__filename));
+// console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));
