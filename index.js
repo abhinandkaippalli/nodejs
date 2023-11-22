e8d49a1afb656fbd62b41297b27cc0527a4c2892
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-22 11:11:11 +0530

    synchronous function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function greet(name) {
-  console.log(`Hello ${name}`);
-}
+// function greet(name) {
+//   console.log(`Hello ${name}`);
+// }
 
-function higherOrderFunction(callback) {
-  const name = "Vishwas";
-  callback(name);
-}
+// function higherOrderFunction(callback) {
+//   const name = "Vishwas";
+//   callback(name);
+// }
 
-higherOrderFunction(greet);
+// higherOrderFunction(greet);
+
+//synchronous function
+let numbers = [1, 2, 4, 8, 5, 3, 7, 6];
+console.log(numbers.sort((a, b) => a - b));
+console.log(numbers.filter((n) => n % 2 === 0));
+console.log(numbers.map((n) => n / 2));
