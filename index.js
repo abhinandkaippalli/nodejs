e97842f501f259026269ff5fd0f687549e16a181
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-22 12:39:46 +0530

    passing arguement

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const EventEmitter = require("node:events")
+const EventEmitter = require("node:events");
 
-const emitter = new EventEmitter()
+const emitter = new EventEmitter();
 
-emitter.on("order-pizza", () => {
-  console.log("Oredr received! Baking a pizza");
-})
+// emitter.on("order-pizza", () => {
+//   console.log("Oredr received! Baking a pizza");
+// })
 
-emitter.emit("order-pizza")
+// emitter.emit("order-pizza")
+
+//passing arguements
+emitter.on("order-pizza", (size, topping) => {
+  console.log(`Oredr received! Baking a ${size} pizza ${topping}`);
+});
+
+emitter.emit("order-pizza", "large", "mushrooms");
