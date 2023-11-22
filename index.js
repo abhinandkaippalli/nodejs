8c31a165951e02223d33bfcd6d8a7d34527aa354
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-22 16:45:16 +0530

    extending event emitter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const PizzaShop = require("./pizza-shop");
+const DrinkMachine = require("./drink-machine");
+
+const pizzaShop = new PizzaShop();
+const drinkMachine = new DrinkMachine();
+
+pizzaShop.on("order", (size, topping) => {
+  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+  drinkMachine.serverDrink(size)
+});
+
+pizzaShop.order("large", "mushrooms");
+pizzaShop.diplayOrderNumber();
