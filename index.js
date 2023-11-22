514989314f396b6f9f2b7432291be48a47f5bd3e
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-22 12:27:30 +0530

    custom event

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const EventEmitter = require("node:events")
+
+const emitter = new EventEmitter()
+
+emitter.on("order-pizza", () => {
+  console.log("Oredr received! Baking a pizza");
+})
+
+emitter.emit("order-pizza")
