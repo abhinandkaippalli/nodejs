d529c541ad33b2665fbe64ce012cc0abc5d457a9
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-23 11:48:39 +0530

    streams and buffer

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const buffer = new Buffer.from("Vishwas");
+
+// buffer.write("Code")
+buffer.write("Codevolution")
+
+console.log(buffer.toString());
+console.log(buffer);
+console.log(buffer.toJSON());
+
