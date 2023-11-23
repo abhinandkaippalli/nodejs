a050a95a4aa7a8e7eb65f5d6d4c1647e70ade115
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-23 15:53:28 +0530

    reading file asynchronously

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const fs = require("node:fs")
 
 const fileContents = fs.readFileSync("./file.txt", "utf-8")
 console.log(fileContents);
- 
+
+fs.readFile("./file.txt", (error, data) => {
+  if(error) {
+    console.log(erro);
+  } else {
+    console.log(data);
+  }
+})
