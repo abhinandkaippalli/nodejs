404507d7c9b8ca907b2166c8818d7ad2f905451a
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-23 17:23:51 +0530

    async await

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const fs = require("node:fs/promises");
 
-fs.readFile("./file.txt", "utf-8")
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+//promise
+// fs.readFile("./file.txt", "utf-8")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
 
-// const fs = require("node:fs");
+//async await
+async function readFile() {
+  try {
+    const data = await fs.readFile("file.txt", "utf-8");
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
-// const fileContents = fs.readFileSync("./file.txt", "utf-8");
-// console.log(fileContents);
-
-// fs.readFile("./file.txt", (error, data) => {
-//   if (error) {
-//     console.log(erro);
-//   } else {
-//     console.log(data);
-//   }
-// });
-
-// fs.writeFileSync("./greet.txt", "Hello world!");
-
-// fs.writeFile("./greet.txt", " Hello vishwas", { flag: "a" }, (err) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("File Written");
-//   }
-// });
+readFile();
