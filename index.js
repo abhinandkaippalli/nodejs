4db1d23f6388657f141814176c123e20037dfa6f
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-24 14:11:26 +0530

    pipe

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require("node:fs");
 
-const buffer = Buffer.alloc(12); // Create a buffer of size 10 bytes
-
 const readableStream = fs.createReadStream("./file.txt", {
   encoding: "utf-8",
-  highWaterMark: buffer.length, //data in chunks of 2 bytes 
+  highWaterMark: 2, //data in chunks of 2 bytes 
 });
 
 const writeableStream = fs.createWriteStream("./file2.txt");
 
-readableStream.on("data", (chunk) => {
-  console.log(chunk);
-  writeableStream.write(chunk);
-});
+readableStream.pipe(writeableStream)
+
+// readableStream.on("data", (chunk) => {
+//   console.log(chunk);
+//   writeableStream.write(chunk);
+// });
