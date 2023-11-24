fd939f18dffead265c1b16839a84f40b8ba6e594
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-24 10:51:52 +0530

    write data to file2.txt

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const readableStream = fs.createReadStream("./file.txt", {
 });
 
 const writeableStream = fs.createWriteStream("./file2.txt");
+
+readableStream.on("data", (chunk) => {
+  console.log(chunk);
+  writeableStream.write(chunk);
+});
