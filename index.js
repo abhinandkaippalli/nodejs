aa051fb230d0d9c452482022fd00446c96cfa405
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-27 10:59:43 +0530

    html response

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "text/html" });
+  res.end("<h1>Hello World</h1>");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
