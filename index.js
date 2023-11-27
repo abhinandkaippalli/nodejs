1078051cebb7b38e96c2b0e9e5be894c7eeb66c2
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-27 15:58:12 +0530

    thread pool

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const crypto = require("node:crypto");
+
+const start = Date.now();
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+console.log("Hash: ", Date.now() - start);
