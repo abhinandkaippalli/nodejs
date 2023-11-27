304b9530f3cbe930f09105b66dfb02defa53de3a
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-27 14:09:58 +0530

    url string

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  const name = "Vishwas"
-  res.writeHead(200, { "content-type": "text/html" });
-  let html = fs.readFileSync("./index.html", "utf-8");
-  html = html.replace("{{name}}", name)
-  res.end(html);
+  res.end(req.url);
 });
 
 server.listen(3000, () => {
