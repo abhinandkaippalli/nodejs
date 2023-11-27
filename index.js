d1109213338e2ad7af41acc36aefc8b496c8dcd9
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-27 12:27:40 +0530

    HTML-template

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+const http = require("node:http");
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+  const name = "Vishwas"
+  res.writeHead(200, { "content-type": "text/html" });
+  let html = fs.readFileSync("./index.html", "utf-8");
+  html = html.replace("{{name}}", name)
+  res.end(html);
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
