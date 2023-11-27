fee4d6e5d5deb5a248e4fd34036d522b1153260d
Abhinand K <abhinandkofficial01@gmail.com>
2023-11-27 11:15:12 +0530

    html file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/html" });
-  res.end("<h1>Hello World</h1>");
+  const html = fs.readFileSync("./index.html", "utf-8");
+  res.end(html);
 });
 
 server.listen(3000, () => {
